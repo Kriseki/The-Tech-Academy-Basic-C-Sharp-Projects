89cac5652d03701ff80da8ba5c042210af2bea4f
Kriseki <41407116+Kriseki@users.noreply.github.com>
2018-10-01 15:33:57 -0700

    Add Acme Accounting drill

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+   {
+       static void Main()
+       {
+        Console.WriteLine("Welcome to Acme Accounting System");
+        Console.WriteLine("Remember, we're \"accounting\" on you!");
+        Console.ReadLine();
+       }
+   }

105f78650027c023f5509045726b938456e9ea73
Kriseki <41407116+Kriseki@users.noreply.github.com>
2018-10-11 09:41:58 -0700

    Upload Teamguess

---
+++ Teamguess/Teamguess/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teamguess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Who is the best MLS team?");
+            string team = Console.ReadLine();
+            bool isGuessed = team == "Timbers";
+            do
+            {
+                switch (team)
+                {
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess again.");
+                        team = Console.ReadLine();
+                        break;
+
+                    case "Sounders":
+                        Console.WriteLine("You are very wrong. You should be embarrassed and ashamed.");
+                        Console.WriteLine("Guess again");
+                        team = Console.ReadLine();
+                        break;
+
+                    case "Timbers":
+                        Console.WriteLine("That's right!");
+                        isGuessed = true;
+                        break;
+                }
+            }
+            while (!isGuessed);
+
+            Console.ReadLine();
+        }
+    }
+}

d85252c522928c8a27b1c8bad38a4bac594e9f5c
Kriseki <41407116+Kriseki@users.noreply.github.com>
2018-10-22 13:11:04 -0700

    Upload method and class drill

---
+++ MethodMan/MethodMan/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodMan
+{
+    public class Math
+    {
+        public int Multiply(int num1)
+        {
+            int result = num1 * 10;
+            return result;
+        }
+
+        public int Addition(int num1)
+        {
+            int result2 = num1 + 10;
+            return result2;
+        }
+
+        public int Subtract(int num1)
+        {
+            int result3 = num1 - 10;
+            return result3;
+        }
+    }
+}
---
+++ MethodMan/MethodMan/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number and let Method Man do some math.");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int ret;
+            int ret2;
+            int ret3;
+            Math n = new Math();
+
+            ret = n.Multiply(a);
+            ret2 = n.Addition(a);
+            ret3 = n.Subtract(a);
+            Console.WriteLine("Your number times 10 = " + ret);
+            Console.WriteLine("Your number plus 10 = " + ret2);
+            Console.WriteLine("Your number minus 10 = " + ret3);
+            Console.ReadLine();
+
+        }
+    }
+}

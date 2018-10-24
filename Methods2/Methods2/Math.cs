1c5320ccc4c7b1cbbad0c1ec6096f6df30a5c05f
Kriseki <41407116+Kriseki@users.noreply.github.com>
2018-10-24 13:22:16 -0700

    Upload Methods2 drill

---
+++ Methods2/Methods2/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodMan
+{
+    public class Math
+    {
+        public void Addition(int a, int b)
+        {
+            int result2 = a + 10;
+            Console.WriteLine(b);
+        }
+
+        
+    }
+}
---
+++ Methods2/Methods2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math n = new Math();
+            n.Addition(10, 30);
+            n.Addition(a: 20, b: 40);
+            Console.ReadLine();
+        }
+    }
+}

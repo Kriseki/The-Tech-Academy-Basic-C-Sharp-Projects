be46fbb4a70a40c963b030b3361e33a4d3b7d8f9
Kriseki <41407116+Kriseki@users.noreply.github.com>
2018-10-24 14:53:29 -0700

    Upload Method4 drill

---
+++ Method4/Method4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math n = new Math();
+
+            Console.WriteLine("Pick a number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick another number if you want or hit enter to use the default number of 5.");
+            string num2 = Console.ReadLine();
+            Console.WriteLine("We will now add the first number to the second number.");
+            if (num2 == "")
+            {
+                Console.WriteLine("Your answer is: " + n.Addition(num1));
+            }
+            else
+            {
+                int num3 = Convert.ToInt32(num2);
+                Console.WriteLine("Your answer is: " + n.Addition(num1, num3));
+            }
+            Console.ReadLine();
+        }
+    }
+}

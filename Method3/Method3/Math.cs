2a1da569590448f70f38685c963fdabdaff5a965
Kriseki <41407116+Kriseki@users.noreply.github.com>
2018-10-24 14:10:32 -0700

    Upload Method3 drill

---
+++ Method3/Method3/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method3
+{
+    public class Math
+    {
+        public int Addition(int num1)
+        {
+            int result = num1 + 10;
+            return result;
+        }
+
+        public int Addition(decimal num2)
+        {
+            decimal result2 = num2 * 2;
+            int result22 = Convert.ToInt32(result2);
+            return result22;
+        }
+
+        public int Addition(string num3)
+        {
+            int num33 = Convert.ToInt32(num3);
+            int result3 = num33 - 5;
+            return result3;
+        }
+    }
+}
---
+++ Method3/Method3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math n = new Math();
+            Console.WriteLine(n.Addition(12));
+            Console.WriteLine(n.Addition(10.5m));
+            Console.WriteLine(n.Addition("10"));
+            Console.ReadLine();
+        }
+    }
+}

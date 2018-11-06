f6760a90baca690238b201e5714913cf9dec61fe
Kriseki <41407116+Kriseki@users.noreply.github.com>
2018-11-06 14:56:23 -0800

    Upload Generics drill

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> employee = new List<string> { "one", "two", "three" };
+            List<int> employee1 = new List<int> { 1, 2, 3 };
+            employee.ForEach(em => Console.WriteLine(em));
+            employee1.ForEach(em => Console.WriteLine(em));
+            Console.ReadLine();
+        }
+    }
+}

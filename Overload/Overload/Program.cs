68651df512f9025d53f6c8d5fac4e02360a75a4c
Kriseki <41407116+Kriseki@users.noreply.github.com>
2018-11-06 12:36:20 -0800

    Upload overload

---
+++ Overload/Overload/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee n = new Employee();
+            n.ID = 001;
+            Employee m = new Employee();
+            m.ID = 002;
+
+            n.GetID(n.ID, m.ID);
+            
+            Console.ReadLine();
+
+        }
+    }
+}

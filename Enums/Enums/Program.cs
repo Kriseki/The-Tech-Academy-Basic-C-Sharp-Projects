b9f00331f61b7ad13247d8ee2e51e841f97d050d
Kriseki <41407116+Kriseki@users.noreply.github.com>
2018-11-06 16:20:20 -0800

    Upload Enums drill

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the current day of the week");
+            string day = Console.ReadLine();
+
+            try
+            {
+                daysOfTheWeek days = (daysOfTheWeek)Enum.Parse(typeof(daysOfTheWeek), day);
+                Console.WriteLine("Today's day is {0}", day);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+            }
+            
+
+
+        }
+        enum daysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}

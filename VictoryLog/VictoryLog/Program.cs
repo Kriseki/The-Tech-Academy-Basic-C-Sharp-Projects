09f7b1757b12ff11210d1ea4eb68e2bf4eff0eac
Kriseki <41407116+Kriseki@users.noreply.github.com>
2018-11-08 14:21:56 -0800

    Upload I/O and Log drill

---
+++ VictoryLog/VictoryLog/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace VictoryLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many goals will the Timbers score tonight?");
+            string answer = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Student\Desktop\Projects\Logs\timbers.txt", answer);
+            Console.WriteLine("We have logged that you predict the Timbers will score " + File.ReadAllText(@"C:\Users\Student\Desktop\Projects\Logs\timbers.txt") + " goals.");
+            Console.ReadLine();
+        }
+    }
+}

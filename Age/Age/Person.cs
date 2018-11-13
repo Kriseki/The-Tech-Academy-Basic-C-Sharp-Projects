60bf5b2bf6bdb1bf04571d6fa54257a3bf7bec4e
Kriseki <41407116+Kriseki@users.noreply.github.com>
2018-11-12 19:49:39 -0800

    Upload Exceptions drill

---
+++ Age/Age/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    public class Person
+    {
+        public int year { get; set; }
+
+        public Person(int theiryear)
+        {
+            if (theiryear <= 0)
+            {
+                throw new AgeException();
+            }
+            else
+            {
+                year = DateTime.Now.Year - theiryear;
+            }
+        }
+    }
+}
---
+++ Age/Age/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("Please enter your age.");
+                int theirAge = Convert.ToInt32(Console.ReadLine());
+                Person n = new Person(theirAge);
+                Console.WriteLine("You were born in the year {0}.", n.year);
+                Console.ReadLine();
+            }
+            catch (AgeException)
+            {
+                Console.WriteLine("You entered a 0 or negative. Please enter a valid age.");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error has occurred. Please contact your System Administrator.");
+                Console.ReadLine();
+                return;
+            }
+            
+        }
+    }
+}

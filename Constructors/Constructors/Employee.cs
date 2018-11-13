a6e4bd83634f0850e7fd7957645aca5bdc6af774
Kriseki <41407116+Kriseki@users.noreply.github.com>
2018-11-12 16:46:17 -0800

    Upload constructors drill

---
+++ Constructors/Constructors/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class Employee
+    {
+        public Employee(string name) : this(name, "lastName")
+        {
+
+        }
+        public Employee(string name, string lastName)
+        {
+            fName = name;
+            lName = lastName;
+        }
+    
+        public string fName { get; set; }
+        public string lName { get; set; }
+    }
+}
---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string companyName = "Faux Co";
+            Console.WriteLine("Welcome to {0}'s company directory.", companyName);
+            Console.WriteLine("Please enter your first name.");
+            var firstName = Console.ReadLine();
+            Console.WriteLine("Please enter your last name.");
+            var lastName = Console.ReadLine();
+
+            Employee employee = new Employee(firstName, lastName);
+            Console.WriteLine("You have entered {0} {1} into the employee directory.",employee.fName, employee.lName);
+            Console.WriteLine();
+            Employee newEmployee = new Employee("Sample");
+            Console.WriteLine("{0} {1} has also been added to the directory.", newEmployee.fName, newEmployee.lName);
+            Console.ReadLine();
+        }
+    }
+}
